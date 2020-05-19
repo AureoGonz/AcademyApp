c2b48bb3539395eb30adad51df3694a6bb68aba6
Aureo Gonzalez <aureogonz@hotmail.com>
2020-05-19 16:21:00 -0500

    Agrego llaves de preferencias a catalogo

---
+++ lib/entities/k_preferences.dart
@@ -0,0 +1,19 @@
+// enum KeyPreferencias {
+//   userEmail, userPassword, appTheme
+// }
+
+// String enumNamePreferences(KeyPreferencias anyEnum){
+//   return anyEnum.toString().split('.')[1];
+// }
+
+// final appPreferencia = {
+//   KeyPreferencias.userEmail : 'aca_usr_cur',
+//   KeyPreferencias.userPassword : 'aca_pas_cur',
+//   KeyPreferencias.appTheme : 'app_theme_data_preference'
+// };
+
+class KeyPReferencias {
+  static const userEmail = 'aca_usr_cur';
+  static const userPassword = 'aca_pas_cur';
+  static const appTheme = 'app_theme_data_preference';
+}
