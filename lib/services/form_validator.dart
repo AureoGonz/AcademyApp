f7d2922b0f91a64ea1919608dc1805746806c2fc
Aureo Gonzalez <aureogonz@hotmail.com>
2020-05-19 17:07:32 -0500

    integro validador de campos

---
+++ lib/services/form_validator.dart
@@ -0,0 +1,26 @@
+class FormValidator {
+  
+  static String validateName(String value) {
+    if (value.length < 1)
+      return 'El campo nombre está vacio';
+    else
+      return null;
+  }
+
+  static String validateMobile(String value) {
+    if (value.length != 10)
+      return 'Los números telefónicos son de 10 dígitos';
+    else
+      return null;
+  }
+
+  static String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Ingresa un email válido';
+    else
+      return null;
+  }
+}
